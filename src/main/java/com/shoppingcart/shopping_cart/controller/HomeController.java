f39e24ac7d38c9e58f04a25eb90ca1aa5b1576f9
Swapnil Bhosale <bswapnil699@gmail.com>
2024-05-18 12:48:05 +0530

    setup the project and navbar

---
+++ src/main/java/com/shoppingcart/shopping_cart/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.shoppingcart.shopping_cart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
